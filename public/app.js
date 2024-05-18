ae6864ae16e33a8f17a75eedbcdcb4967ee6467f
idiakeg <idiakeg@gmail.com>
2024-05-18 14:33:39 +0100

    trying to fix database persistence.

--- public/app.js
+++ public/app.js
@@ -4,44 +4,44 @@ let form = document.getElementById("form");
 let phone = document.getElementById("phone_number");
 
 inputField.forEach((item) => {
-	item.addEventListener("input", function (event) {
-		let inputValue = event.target.value;
+    item.addEventListener("input", function (event) {
+        let inputValue = event.target.value;
 
-		// Remove any numbers from the input value
-		let textOnlyValue = inputValue.replace(/[0-9]/g, "");
+        // Remove any numbers from the input value
+        let textOnlyValue = inputValue.replace(/[0-9]/g, "");
 
-		// Update the input field value with the text-only value
-		event.target.value = textOnlyValue;
-	});
+        // Update the input field value with the text-only value
+        event.target.value = textOnlyValue;
+    });
 });
 
 let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	if (!emailRegex.test(email.value)) {
-		alert("Please enter a valid email address.");
-	}
+    e.preventDefault();
+    if (!emailRegex.test(email.value)) {
+        alert("Please enter a valid email address.");
+    }
 
-	if (phone.value.length !== 11) {
-		alert("Phone number should be exactly 11 numbers");
-		return;
-	}
+    if (phone.value.length !== 11) {
+        alert("Phone number should be exactly 11 numbers");
+        return;
+    }
 
-	let fd = new FormData(form);
-	// convert form data to javascript objects
-	let data = Object.fromEntries(fd);
+    let fd = new FormData(form);
+    // convert form data to javascript objects
+    let data = Object.fromEntries(fd);
 
-	fetch("http://localhost:4343/", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	})
-		.then((res) => res.json())
-		.then((data) => console.log(data))
-		.catch((err) => console.log(err));
+    fetch("https://zuri-static-server.vercel.app/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
 
-	window.location = "/success.html";
+    window.location = "/success.html";
 });
